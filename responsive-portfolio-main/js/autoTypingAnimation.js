3a11a590498ab4e21c10b5d84575ba285693a1d3
VIKAS VERMA <122379093+Vikasv06@users.noreply.github.com>
2023-06-06 13:54:59 +0530

    Add files via upload

---
+++ responsive-portfolio-main/js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Vikas Verma</span> My name is Vikas Verma, and I hail from Gorakhpur. I completed my B.Tech from Madan Mohan Malviya University of Technology in 2019. I am passionate about space exploration and have always been intrigued by the mysteries of the universe. I enjoy immersing myself in documentaries, articles, and books that delve into the depths of outer space.<br><br> Aside from space exploration, I find solace in visiting hilly places and embarking on adventurous journeys. The serene beauty and tranquility of mountains captivate me, providing a much-needed escape from the everyday hustle and bustle. I am an avid traveler and take every opportunity to explore new destinations, immerse myself in different cultures, and broaden my horizons.Professionally, I am a full stack Python developer with expertise in web development. I have hands-on experience with Python frameworks such as Django and Flask, along with proficiency in front-end technologies like HTML, CSS, and JavaScript. I possess a solid understanding of databases, including SQL and MongoDB. I am committed to delivering efficient and robust web solutions while continuously enhancing my skills to stay up-to-date with emerging technologies and industry trends.Overall, I am a curious and passionate individual, driven by my love for space, exploration, and technology. I strive to make a positive impact through my work and continuously seek opportunities for personal and professional growth."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
